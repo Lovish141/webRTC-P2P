5f671478edf129ddde638309055727d3ec1afd91
Lovish Sharma <slovish11sharma@gmail.com>
2024-06-06 20:15:36 +0530

    Backend init

--- client/src/Components/Room.tsx
+++ client/src/Components/Room.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import { io } from "socket.io-client";
+const URL="http://localhost:3000";
 
 const Room=()=>{
     const [searchParam,setSearchParam]=useSearchParams();
@@ -7,6 +9,29 @@ const Room=()=>{
 
     useEffect(()=>{
         //Logic to join the room
+        const socket=io(URL,{
+            autoConnect:false
+        })
+
+        socket.on("send-offer",({roomId})=>{
+            alert("Send an offer please");
+            socket.emit("offer",{
+                roomId,
+                sdp:""
+            })
+        })
+
+        socket.on("offer",({roomId})=>{
+            alert("Send answer please");
+            socket.emit("answer",{
+                roomId,
+                sdp:""
+            })
+        })
+
+        socket.on("answer",()=>{
+            alert("Connection done");
+        })
     },
     [name])
     return(
